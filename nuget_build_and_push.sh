f558dc28baea503136d10058255835eb3d04f489
slafniy <slafniy@gmail.com>
2026-07-31 06:43:29 +0200

    bring back build script

---
+++ nuget_build_and_push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION="1.0.3"
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
+
+dotnet pack LibMSPSharp/LibMSPSharp.csproj -c Release /p:Version="${VERSION}"
+
+
+# Should have an active github classic token with "package write" permission
+dotnet nuget push "${SCRIPT_DIR}/LibMSPSharp/bin/Release/slafniy.LibMSPSharp.${VERSION}.nupkg" \
+  --source "https://nuget.pkg.github.com/slafniy/index.json" \
+  --api-key $(secret-tool lookup github nuget)
