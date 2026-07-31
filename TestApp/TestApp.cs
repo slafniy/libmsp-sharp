2ff5e0b2bfd308b00b3971c977e4c9802e07c039
slafniy <slafniy@gmail.com>
2026-07-31 02:34:20 +0200

    rolled back nuget nonsense

--- TestApp/TestApp.cs
+++ TestApp/TestApp.cs
@@ -1,17 +1,14 @@
 ﻿using LibMSPSharp;
 
-using var player = new LibMSP("/mnt/data/code/libmsp/cmake-build-release/libmsp.so");
+using var player = new LibMSP(args[0]);
 
-foreach ((string k, string? v) in player.GetMetadata(
-             @"/mnt/data/Music/Avatar/2023 - Dance Devil Dance/01. Dance Devil Dance.mp3",
-             ["artist", "title", "album", "date"]
-         )) {
+foreach ((string k, string? v) in player.GetMetadata(args[1],["artist", "title", "album", "date"])) {
     Console.Write($"{k}: {v}; ");
 }
 
 Console.WriteLine();
 
 
-player.Play(@"/mnt/data/Music/Avatar/2023 - Dance Devil Dance/01. Dance Devil Dance.mp3");
+player.Play(args[1]);
 
 Thread.Sleep(5000);
